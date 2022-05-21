9848eb1a613a8bbe1f5bd580065b7a741053fa40
Ejiks <ivanbirulia@gmail.com>
2022-05-21 13:12:42 +0300

    fix ignore

--- auth_data.py
+++
@@ -1,6 +0,0 @@
-vk_login = 'Ivan Birulia'
-vk_password = '~~DElphi098'
-api_group_key = '506711469a121e0afdc17d5ecfc99b2016a842863b4fc0e82105e1530c43ca942920bfccd97bf96ee19f0'
-VK_USER_ID = '308987349'
-VK_TOKEN = 'eb3327badd86e96c70f36ac172a01d81d1699494a7aa754da618dde88c4e37c6666d323e642c7a2bed953'
-v = 5.131
