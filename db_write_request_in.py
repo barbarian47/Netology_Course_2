049b857b4e0dfe4060f2717bd042202f1bad3539
Ejiks <ivanbirulia@gmail.com>
2022-05-25 22:15:26 +0300

    fix blacklist

--- db_write_request_in.py
+++ db_write_request_in.py
@@ -53,7 +53,7 @@ def write_in_bd(id_client, user_info):
                                 ({id_partner}, '{link}', {iter[0]}, '{iter[1]})');"""
                     )
                     print("[INFO]PostgreSQL vk_user_list write write_link_photo")
-            except:
+            except Exception as _ex:
                 cursor.execute(
                     f"""INSERT INTO list_id(id_vk, id_user_vk, in_black_list) 
                     VALUES ({id_partner}, {id_client}, FALSE);"""
@@ -75,9 +75,7 @@ def write_in_bd(id_client, user_info):
             return
 
 
-def write_in_blacklist(client_id, user_id):
-    id_client = client_id
-    id_partner = user_id
+def write_in_blacklist(id_client, id_partner):
     try:
         # коннектимся к БД
         connection = psycopg2.connect(
@@ -96,11 +94,15 @@ def write_in_blacklist(client_id, user_id):
                 f"[INFO]=====>Connected PostgreSQL vk_user_list {cursor.fetchone()}")
             try:
                 cursor.execute(
+                    f"""INSERT INTO list_user_param(id_vk) 
+                    VALUES ({id_partner});"""
+                )
+                cursor.execute(
                     f"""INSERT INTO list_id(id_vk, id_user_vk, in_black_list) 
                     VALUES ({id_partner}, {id_client}, TRUE);"""
                 )
                 print("[INFO]PostgreSQL vk_user_list write write_list_id")
-            except:
+            except Exception as _ex:
                 cursor.execute(
                     f"""UPDATE list_id
                     SET in_black_list = TRUE
